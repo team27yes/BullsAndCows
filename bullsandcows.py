1ae26aa1bbbf6a655e3747031e1958b8efa66577
‘minsu0919’ <kmsb0919@naver.com>
2023-05-02 17:15:09 +0900

    완성본 데모1

--- bullsandcows.py
+++ bullsandcows.py
@@ -3,17 +3,21 @@ def get_num():
     return random.sample(range(0, 10), 4)
 
 A = get_num()
-num = list(map(int, input("숫자 0부터 9까지 네개 입력하세요: ").split()))
-print(num)
 
-# bulls code
-if A == num:
-    print("정답입니다.")
-else:
+num = []
+while A != num:
+    num = list(map(int, input("숫자 0부터 9까지 네개 입력하세요: ").split()))
+    if (len(num) != 4) or (len(set(num)) != 4) : 
+        print("올바른 값을 입력하세요")
+        continue
     bulls = 0
     cows = 0
     for i in range (len(A)):
         if num[i] == A[i]:
-            bulls = bulls + 1
-    print(f"Bulls : {bulls}")
-#1
+            bulls += 1
+            cows -= 1
+    cows_result = set(num) & set(A)
+    cows += len(cows_result)
+    print(f"Bulls : {bulls}, cows : {cows}")
+
+print("정답")
