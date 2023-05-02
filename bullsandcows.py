76931f7011457e94bf67f2414ffdfa9d3263aa21
Chungxhung <loveiesschung98@gmail.com>
2023-05-02 15:45:20 +0900

    fix: add bulls code

--- bullsandcows.py
+++ bullsandcows.py
@@ -5,4 +5,14 @@ def get_num():
 A = get_num()
 num = list(map(int, input("숫자 0부터 9까지 네개 입력하세요: ").split()))
 print(num)
-#1
+
+# bulls code
+if A == num:
+    print("정답입니다.")
+else:
+    bulls = 0
+    cows = 0
+    for i in range (len(A)):
+        if num[i] == A[i]:
+            bulls = bulls + 1
+    print(f"Bulls : {bulls}")
